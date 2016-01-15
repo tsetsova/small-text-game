2c6e8add5f8f8721f9930f83ba164d3b6d8c1a78
Ina Tsetsova <inatsetsova@Inas-MacBook-Pro.local>
2016-01-15 12:26:26 +0000

    added damage

--- lib/stabby.rb
+++ lib/stabby.rb
@@ -1,3 +1,41 @@
-def stabs(number)
-  "You're stabbed #{number.to_s} time" + "#{"s" if number > 1}."
+def random_number_generator(number)
+  Proc.new{(0..number).map{|num| num}.sample}
 end
+
+class Knife_Fight
+  def initialize
+   @number = random_number_generator(10).call
+   @health = health
+   @damage = (random_number_generator(10).call) * @number
+  end
+
+  attr_reader :number, :damage
+
+  def health
+    health = 300
+  end
+
+  def damage_caused
+    "You've lost #{damage.to_s} hp!"
+  end
+
+  def stabs
+    return "You're stabbed #{number.to_s} time" + "#{"s" if number > 1}." if number != 0
+    "You manage to run away unscathed."
+  end
+end
+
+
+
+attacker = Knife_Fight.new
+
+puts attacker.stabs
+puts attacker.damage_caused
+
+
+#add a character class with a life count
+#add damage to stabs
+#make sure character can die
+#find a way for character to fight back?
+#give attaker ability to die
+#add luck modifier
--- spec/stabby_spec.rb
+++ spec/stabby_spec.rb
@@ -1,8 +1,26 @@
 require "stabby"
 
 describe "stabby" do
-  it "stabs" do
-    number = (2..5).map {|num| num}.sample
-    expect(stabs(number)).to eq "You're stabbed #{number} times."
+
+  attacker = Knife_Fight.new
+
+  it "stabs 4 times if number == 4" do
+    allow(attacker).to receive(:number).and_return(4)
+    expect(attacker.stabs).to eq "You're stabbed 4 times."
+  end
+
+
+  it "runs away if number == 0" do
+    allow(attacker).to receive(:number).and_return(0)
+    expect(attacker.stabs).to eq "You manage to run away unscathed."
+  end
+
+  it "returns health of attacker" do
+    expect(attacker.health).to eq 300
+  end
+
+   it "damages 200 hp if damage is 200" do
+    allow(attacker).to receive(:damage).and_return(200)
+    expect(attacker.damage_caused).to eq "You've lost 200 hp!"
   end
 end
